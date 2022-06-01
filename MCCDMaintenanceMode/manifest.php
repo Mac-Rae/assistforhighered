957dd1b6e22a6e66603c98fb4bb800bba58a18e1
Alcides Ojeda <eojedapilchik@gmail.com>
2022-06-01 16:14:37 +0100

    Update package branding for the Maintenance plugin

--- MCCDMaintenanceMode/manifest.php
+++ MCCDMaintenanceMode/manifest.php
@@ -18,35 +18,35 @@ $manifest = [
     'description' => 'Maintenance Mode Plugin',
     'icon' => '',
     'is_uninstallable' => '1',
-    'name' => 'MCCD Maintenance Mode',
+    'name' => 'ASSIST Maintenance Mode',
     'published_date' => '2021-02-18 15:14:28',
     'type' => 'module',
-    'version' => '2',
+    'version' => '3',
     'remove_tables' => 'prompt',
     ];
 $installdefs = [
     'copy' => [
-        '0' => [
+        [
             'from' => '<basepath>/custom/Extension/application/Ext/LogicHooks/SAMaintenanceMode.php',
             'to' => 'custom/Extension/application/Ext/LogicHooks/SAMaintenanceMode.php',
             ],
-        '1' => [
+        [
             'from' => '<basepath>/custom/Extension/modules/Administration/Ext/Administration/MaintenanceMode.php',
             'to' => 'custom/Extension/modules/Administration/Ext/Administration/MaintenanceMode.php',
             ],
-        '2' => [
+        [
             'from' => '<basepath>/custom/Extension/modules/Administration/Ext/Language/en_us.MaintenanceMode.php',
             'to' => 'custom/Extension/modules/Administration/Ext/Language/en_us.MaintenanceMode.php',
             ],
-        '3' => [
+        [
             'from' => '<basepath>/custom/include/Services/SAMaintenance.php',
             'to' => 'custom/include/Services/SAMaintenance.php',
             ],
-        '4' => [
+        [
             'from' => '<basepath>/custom/modules/Administration/MaintenanceMode.php',
             'to' => 'custom/modules/Administration/MaintenanceMode.php',
             ],
-        '5' => [
+        [
             'from' => '<basepath>/custom/modules/Administration/MaintenanceMode.tpl',
             'to' => 'custom/modules/Administration/MaintenanceMode.tpl',
             ],
