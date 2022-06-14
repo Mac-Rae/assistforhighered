f6e605e9f5361cbee4a47950e1e8a582aadac1c7
Jim Mackin <jim.mackin@salesagility.com>
2022-06-14 16:46:51 +0100

    Theme: V30

--- ASSISTThemePackage/manifest.php
+++ ASSISTThemePackage/manifest.php
@@ -21,264 +21,268 @@ $manifest = [
     'name' => 'Maricopa Community Colleges SuiteCRM Theme',
     'published_date' => '2021-01-26 09:20:21',
     'type' => 'module',
-    'version' => '29',
+    'version' => '30',
     'remove_tables' => 'prompt',
     ];
 $installdefs = [
     'copy' => [
-        '0' => [
+        [
             'from' => '<basepath>/custom/Extension/application/Ext/GlobalLinks/ASSISTTheme.php',
             'to' => 'custom/Extension/application/Ext/GlobalLinks/ASSISTTheme.php',
             ],
-        '1' => [
+        [
             'from' => '<basepath>/custom/Extension/application/Ext/Language/en_us.ASSISTTheme.php',
             'to' => 'custom/Extension/application/Ext/Language/en_us.ASSISTTheme.php',
             ],
-        '2' => [
+        [
             'from' => '<basepath>/custom/Extension/modules/Administration/Ext/Administration/MaricopaTheme.php',
             'to' => 'custom/Extension/modules/Administration/Ext/Administration/MaricopaTheme.php',
             ],
-        '3' => [
+        [
             'from' => '<basepath>/custom/Extension/modules/Administration/Ext/Language/en_us.MaricopaTheme.php',
             'to' => 'custom/Extension/modules/Administration/Ext/Language/en_us.MaricopaTheme.php',
             ],
-        '4' => [
+        [
             'from' => '<basepath>/custom/Extension/modules/Users/Ext/Language/en_us.ASSISTTheme.php',
             'to' => 'custom/Extension/modules/Users/Ext/Language/en_us.ASSISTTheme.php',
             ],
-        '5' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/LICENSE.md',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/LICENSE.md',
             ],
-        '6' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/README.md',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/README.md',
             ],
-        '7' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/bin/pscss',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/bin/pscss',
             ],
-        '8' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/composer.json',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/composer.json',
             ],
-        '9' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/phpcs.xml.dist',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/phpcs.xml.dist',
             ],
-        '10' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/scss.inc.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/scss.inc.php',
             ],
-        '11' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Base/Range.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Base/Range.php',
             ],
-        '12' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Block.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Block.php',
             ],
-        '13' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Cache.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Cache.php',
             ],
-        '14' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Colors.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Colors.php',
             ],
-        '15' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/CompilationResult.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/CompilationResult.php',
             ],
-        '16' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Compiler.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Compiler.php',
             ],
-        '17' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Compiler/CachedResult.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Compiler/CachedResult.php',
             ],
-        '18' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Compiler/Environment.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Compiler/Environment.php',
             ],
-        '19' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Exception/CompilerException.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Exception/CompilerException.php',
             ],
-        '20' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Exception/ParserException.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Exception/ParserException.php',
             ],
-        '21' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Exception/RangeException.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Exception/RangeException.php',
             ],
-        '22' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Exception/SassException.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Exception/SassException.php',
             ],
-        '23' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Exception/SassScriptException.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Exception/SassScriptException.php',
             ],
-        '24' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Exception/ServerException.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Exception/ServerException.php',
             ],
-        '25' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Formatter.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Formatter.php',
             ],
-        '26' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Formatter/Compact.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Formatter/Compact.php',
             ],
-        '27' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Formatter/Compressed.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Formatter/Compressed.php',
             ],
-        '28' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Formatter/Crunched.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Formatter/Crunched.php',
             ],
-        '29' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Formatter/Debug.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Formatter/Debug.php',
             ],
-        '30' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Formatter/Expanded.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Formatter/Expanded.php',
             ],
-        '31' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Formatter/Nested.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Formatter/Nested.php',
             ],
-        '32' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Formatter/OutputBlock.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Formatter/OutputBlock.php',
             ],
-        '33' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Logger/LoggerInterface.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Logger/LoggerInterface.php',
             ],
-        '34' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Logger/QuietLogger.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Logger/QuietLogger.php',
             ],
-        '35' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Logger/StreamLogger.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Logger/StreamLogger.php',
             ],
-        '36' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Node.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Node.php',
             ],
-        '37' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Node/Number.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Node/Number.php',
             ],
-        '38' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/OutputStyle.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/OutputStyle.php',
             ],
-        '39' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Parser.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Parser.php',
             ],
-        '40' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/SourceMap/Base64.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/SourceMap/Base64.php',
             ],
-        '41' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/SourceMap/Base64VLQ.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/SourceMap/Base64VLQ.php',
             ],
-        '42' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/SourceMap/SourceMapGenerator.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/SourceMap/SourceMapGenerator.php',
             ],
-        '43' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Type.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Type.php',
             ],
-        '44' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Util.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Util.php',
             ],
-        '45' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Util/Path.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Util/Path.php',
             ],
-        '46' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/ValueConverter.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/ValueConverter.php',
             ],
-        '47' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Version.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Version.php',
             ],
-        '48' => [
+        [
             'from' => '<basepath>/custom/include/ASSISTThemeLib/scssphp/src/Warn.php',
             'to' => 'custom/include/ASSISTThemeLib/scssphp/src/Warn.php',
             ],
-        '49' => [
+        [
             'from' => '<basepath>/custom/include/generic/SugarWidgets/SugarWidgetSubPanelTopComposeEmailButton.php',
             'to' => 'custom/include/generic/SugarWidgets/SugarWidgetSubPanelTopComposeEmailButton.php',
             ],
-        '50' => [
+        [
             'from' => '<basepath>/custom/modules/Administration/MaricopaTheme.php',
             'to' => 'custom/modules/Administration/MaricopaTheme.php',
             ],
-        '51' => [
+        [
             'from' => '<basepath>/custom/modules/Administration/MaricopaTheme.tpl',
             'to' => 'custom/modules/Administration/MaricopaTheme.tpl',
             ],
-        '52' => [
+        [
             'from' => '<basepath>/custom/themes/SuiteP/images/accept_inline.svg',
             'to' => 'custom/themes/SuiteP/images/accept_inline.svg',
             ],
-        '53' => [
+        [
             'from' => '<basepath>/custom/themes/SuiteP/images/decline_inline.svg',
             'to' => 'custom/themes/SuiteP/images/decline_inline.svg',
             ],
-        '54' => [
+        [
             'from' => '<basepath>/custom/themes/SuiteP/images/sugar_icon.ico',
             'to' => 'custom/themes/SuiteP/images/sugar_icon.ico',
             ],
-        '55' => [
+        [
             'from' => '<basepath>/custom/themes/SuiteP/images/tentative_inline.svg',
             'to' => 'custom/themes/SuiteP/images/tentative_inline.svg',
             ],
-        '56' => [
+        [
             'from' => '<basepath>/custom/themes/SuiteP/themedef.php',
             'to' => 'custom/themes/SuiteP/themedef.php',
             ],
-        '57' => [
+        [
+            'from' => '<basepath>/custom/themes/SuiteP/tpls/_headerModuleList.tpl',
+            'to' => 'custom/themes/SuiteP/tpls/_headerModuleList.tpl',
+            ],
+        [
             'from' => '<basepath>/custom/themes/default/images/company_logo.png',
             'to' => 'custom/themes/default/images/company_logo.png',
             ],
-        '58' => [
+        [
             'from' => '<basepath>/include/SubPanel/SubPanelTiles.php',
             'to' => 'include/SubPanel/SubPanelTiles.php',
             ],
-        '59' => [
+        [
             'from' => '<basepath>/themes/SuiteP/css/ASSIST/color-palette.scss',
             'to' => 'themes/SuiteP/css/ASSIST/color-palette.scss',
             ],
-        '60' => [
+        [
             'from' => '<basepath>/themes/SuiteP/css/ASSIST/icons.scss',
             'to' => 'themes/SuiteP/css/ASSIST/icons.scss',
             ],
-        '61' => [
+        [
             'from' => '<basepath>/themes/SuiteP/css/ASSIST/style.css',
             'to' => 'themes/SuiteP/css/ASSIST/style.css',
             ],
-        '62' => [
+        [
             'from' => '<basepath>/themes/SuiteP/css/ASSIST/style.scss',
             'to' => 'themes/SuiteP/css/ASSIST/style.scss',
             ],
-        '63' => [
+        [
             'from' => '<basepath>/themes/SuiteP/css/ASSIST/variables.scss',
             'to' => 'themes/SuiteP/css/ASSIST/variables.scss',
             ],
